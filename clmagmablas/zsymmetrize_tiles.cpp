a432b1eba8400dd7336994b0d04d0c617a421243
octaveoclx <tjcgy@hotmail.com>
2026-05-02 03:14:36 +0800

    Initialize 'arg' to 0 in zsymmetrize_tiles.cpp

--- clmagmablas/zsymmetrize_tiles.cpp
+++ clmagmablas/zsymmetrize_tiles.cpp
@@ -74,7 +74,7 @@ magmablas_zsymmetrize_tiles(
 {
     cl_kernel kernel;
     cl_int err;
-    int arg;
+    int arg = 0;
 
     magma_int_t info = 0;
     if ( uplo != MagmaLower && uplo != MagmaUpper )
