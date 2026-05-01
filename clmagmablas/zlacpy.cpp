2ea2f00b605dcc994a92c6e196ef606e97064160
octaveoclx <tjcgy@hotmail.com>
2026-05-02 03:18:32 +0800

    Update zlacpy.cpp

--- clmagmablas/zlacpy.cpp
+++ clmagmablas/zlacpy.cpp
@@ -82,7 +82,7 @@ magmablas_zlacpy(
 {
     cl_kernel kernel;
     cl_int err;
-    int arg;
+    int arg = 0;
 
     magma_int_t info = 0;
     if ( uplo != MagmaLower && uplo != MagmaUpper && uplo != MagmaFull )
